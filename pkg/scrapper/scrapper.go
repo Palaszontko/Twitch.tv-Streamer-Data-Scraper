5a1e5c4dbabf366eb41af34af8696b8c019c3237
Olgierd Palasz <olgierd.palasz@gmail.com>
2024-03-23 00:49:05 +0100

    feat: scrapper with streamer data functionality

--- pkg/scrapper/scrapper.go
+++ pkg/scrapper/scrapper.go
@@ -1,17 +1,52 @@
 package scrapper
 
 import (
+	"fmt"
 	"log"
 	"net/http"
-	"fmt"
+
 	"github.com/PuerkitoBio/goquery"
 )
 
-func Scrapper() {
-	log := log.New(log.Writer(), "Log:", log.Ltime)
-	log.Printf("Starting...")
+func scrapper(nickname string, timePeriod int) StreamerData {
+	streamer := StreamerData{
+		Nickname: nickname,
+	}
+	scrapperStreamerData(streamer.Nickname, timePeriod, &streamer)
 
-	res, err := http.Get("https://sullygnome.com/channel/xqc/30")
+	logger.Println("Streamer data scrapped.")
+	return streamer
+}
+
+func Start() {
+	var streamersDataArray []StreamerData
+
+	sessionId := sessionId()
+	loggerInit(sessionId)
+	timePeriod := readConfig().TimePeriod
+
+	logger.Println("Scrapper started.")
+
+	nicknames := inputReader()
+
+	for i, nickname := range nicknames {
+		logger.Println("Scrapper started for: ", nickname, "(", i+1, "/", len(nicknames), ")")
+		scrapedStreamer := scrapper(nickname, timePeriod)
+		streamersDataArray = append(streamersDataArray, scrapedStreamer)
+	}
+
+	logger.Println("Scrapper finished.")
+	outputWriter(streamersDataArray, sessionId)
+
+}
+
+/*
+This function scraps the streamer data from the website.
+With we can fulfill all the fields of the StreamerData struct except for the GamesPlayed field.
+*/
+func scrapperStreamerData(nickname string, timePeriod int, streamer *StreamerData) {
+	url := fmt.Sprintf("https://sullygnome.com/channel/%s/%d", nickname, timePeriod)
+	res, err := http.Get(url)
 
 	if err != nil {
 		log.Fatalln("Error: ", err)
@@ -29,10 +64,25 @@ func Scrapper() {
 		log.Fatal("Error while reading HTML:", err)
 	}
 
-	doc.Find(".InfoStatPanelInner").Each(func(i int, s *goquery.Selection) {
-		title := s.Find(".InfoStatPanelTL").AttrOr("title", "No title found")
+	/*
+		Followers and Language
+	*/
+	doc.Find(".MiddleSubHeaderRow").Each(func(i int, s *goquery.Selection) {
+		followersDiv := s.Find("[title='Total number of followers']")
+		followersValue := parseValue(followersDiv.Next().Text())
 
-		fmt.Println("Title:", title)
+		languageDiv := s.Find("[title='Broadcast language']")
+		languageValue := languageDiv.Next().Text()
+
+		streamer.Language = languageValue
+		streamer.Followers = followersValue
 	})
-
+	/*
+		Streams data
+	*/
+	doc.Find("[class*='InfoStatPanelWrapper']").Each(func(i int, s *goquery.Selection) {
+		fieldName := s.Find(".InfoStatPanelBRCell").Text()
+		fieldValue := s.Find(".InfoStatPanelTLCell").Text()
+		setFiled(streamer, parseName(fieldName), parseValue(fieldValue))
+	})
 }
