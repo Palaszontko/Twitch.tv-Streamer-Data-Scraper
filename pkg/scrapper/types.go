d3a3c98e6de51bdcf9f94d76934cddcac0389a5c
Olgierd Palasz <olgierd.palasz@gmail.com>
2024-03-23 02:55:29 +0100

    feat: data structs

---
+++ pkg/scrapper/types.go
@@ -0,0 +1,42 @@
+package scrapper
+
+import (
+	"reflect"
+)
+
+type StreamerData struct {
+	Nickname        string       `json:"nickname"`
+	Followers       int          `json:"followers"`
+	Language        string       `json:"language"`
+	AverageViewers  int          `json:"averageViewers"`
+	HoursWatched    int          `json:"hoursWatched"`
+	FollowersGained int          `json:"followersGained"`
+	PeakViewers     int          `json:"peakViewers"`
+	HoursStreamed   int          `json:"hoursStreamed"`
+	Streams         int          `json:"streams"`
+	GamesPlayed     []GamePlayed `json:"gamesPlayed"`
+}
+
+type GamePlayed struct {
+	GameName       string `json:"gameName"`
+	StreamTime     int    `json:"streamTime"`
+	AverageViewers int    `json:"averageViewers"`
+	PeakViewers    int    `json:"peakViewers"`
+}
+
+func setFiled(streamer *StreamerData, field string, value interface{}) {
+	streamerReflect := reflect.ValueOf(streamer).Elem()
+	fieldReflect := streamerReflect.FieldByName(field)
+
+	if fieldReflect.IsValid() {
+		switch v := value.(type) {
+		case string:
+			fieldReflect.SetString(v)
+		case int:
+			fieldReflect.SetInt(int64(v))
+		default:
+			logger.Println("Unknown type: ", v)
+		}
+	}
+
+}
