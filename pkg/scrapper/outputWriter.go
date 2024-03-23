1510468b9c3814cdcfbbda16e8ac3b628214a598
Olgierd Palasz <olgierd.palasz@gmail.com>
2024-03-23 17:03:06 +0100

    feat: json output writer for whole data

---
+++ pkg/scrapper/outputWriter.go
@@ -0,0 +1,36 @@
+package scrapper
+
+import (
+	"encoding/json"
+	"os"
+	"time"
+)
+
+func outputWriter(streamersDataArray []StreamerData, sessionId string) {
+	file, err := os.OpenFile("data/output/output_" + sessionId + ".json", os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0644)
+
+	if err != nil {
+		logger.Println("Error while opening the file: ", err)
+	}
+
+	defer file.Close()
+
+	jsonData, err := json.MarshalIndent(streamersDataArray, "", "    ")
+	
+	if err != nil {
+		logger.Println("Error while marshaling the data: ", err)
+	}
+
+	_, err = file.Write(jsonData)
+
+	if err != nil {
+		logger.Println("Error while writing the data: ", err)
+	}
+
+	logger.Println("Data written to the file.")
+}
+
+func sessionId() string {
+	sessionId := time.Now().Format("2006-01-02 15_04_05")
+	return sessionId
+}
